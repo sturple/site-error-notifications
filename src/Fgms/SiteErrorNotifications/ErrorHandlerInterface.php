c679bf39a97695037ad4db97d9c2ed0fbcb13ed0
Robert Allan Hennigan Leahy <rleahy@rleahy.ca>
2016-10-07 01:56:13 -0700

    \Fgms\SiteErrorNotifications\ErrorHandlerInterface Documentation

--- src/Fgms/SiteErrorNotifications/ErrorHandlerInterface.php
+++ src/Fgms/SiteErrorNotifications/ErrorHandlerInterface.php
@@ -8,6 +8,26 @@ namespace Fgms\SiteErrorNotifications;
  */
 interface ErrorHandlerInterface
 {
+    /**
+     * Invoked on a PHP error.
+     *
+     * @param int $errno
+     * @param string $errstr
+     * @param string $errfile
+     * @param int $errline
+     * @param array $errcontext
+     *
+     * @return bool
+     */
     public function error($errno, $errstr, $errfile, $errline, array $errcontext);
+
+    /**
+     * Invoked on an uncaught exception.
+     *
+     * @param $ex
+     *  The uncaught exception.  In PHP 5 this must be
+     *  an Exception object.  In PHP 7 this must be a
+     *  Throwable object.
+     */
     public function uncaught($ex);
 }
